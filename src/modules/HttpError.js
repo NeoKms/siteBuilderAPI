db7a1a2de8bf1db13047b5b0f7a9c5a060d6a60e
neokms <upachko@gmail.com>
2022-05-10 22:30:56 +0300

    Create HttpError.js

---
+++ src/modules/HttpError.js
@@ -0,0 +1,8 @@
+module.exports = class HttpError extends Error {
+    statusCode = 400
+    constructor(message, sc = 400) {
+        super(message);
+        this.name = "HttpError";
+        this.statusCode = sc;
+    }
+}
